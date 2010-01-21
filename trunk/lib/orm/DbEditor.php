88adf1217331d34bdfe736bb9b5fc1a4024d94e2
cjastram <cjastram@918a0ca0-fd7c-11de-b9d1-2d015cf73129>
2010-01-21 16:42:59 +0000

    Adding foreign key support to editing records, adding delete w/ confirmation support

--- trunk/lib/orm/DbEditor.php
+++ trunk/lib/orm/DbEditor.php
@@ -10,12 +10,14 @@ class DbEditor
 	{
 		$this->dbObject = $dbObject;
 		$this->db = $dbObject->getDatabase();
-
+				
 		$this->postURL = $_SERVER["SCRIPT_NAME"];
+        
+        $table = $dbObject->getTable();
+        $schema = $dbObject->getSchema();
 
-		$scriptName = str_replace( ".php", "", strtolower( basename( $_SERVER["SCRIPT_NAME"] ) ) );
-		$className = strtolower( get_class( $this ) );
-		
+		//$scriptName = str_replace( ".php", "", strtolower( basename( $_SERVER["SCRIPT_NAME"] ) ) );
+		//$className = strtolower( get_class( $this ) );
 		//if ( $scriptName == $className )
 		//{
 			$operation = "catalog";
@@ -25,10 +27,8 @@ class DbEditor
 			switch( $operation )
 			{
 			case "new":
-				$db = $this->db;
-				$table = $dbObject->getTable();
-				$schema = $dbObject->getSchema();
 
+                /*
 				$sql = sprintf( "INSERT INTO `%s` VALUES ()", $table );
 				$db->query( $sql );
 				$sql = sprintf( "SELECT * FROM %s WHERE %s = '%s'",
@@ -36,25 +36,48 @@ class DbEditor
 					$db->value( "SELECT last_insert_id()" ) );
 				$row = $db->row( $sql );
 				$_REQUEST['key'] = $this->getPrimaryKeyValueEncoded( $row );
-
-			case "edit":
+                */
+				
+				$this->showEditor();
+                break;
+			
+            case "edit":
 				$this->showEditor( $_REQUEST["key"] );
 				break;
+
+			case "delete":
+				$this->deleteRecord( $_REQUEST["key"] );
+				$this->showCatalog();
+                break;
 
 			case "save":
 				foreach( $_REQUEST["records"] as $table => $row )
 				{
-					$dbObject->update( $row );
-					#foreach( $row as $key => $data )
-					#{
-						#foreach( $data as $field => $value )
-						#{
-						#}
-					#}
-					#$key = $row
-					#print "<pre>";
-					#print_r($row);
+                    $insert = false;
+                    $primaryKeyFields = array();
+                    foreach( $row as $encoded => $array )
+                    {
+                        if( $encoded == "new" )
+                        {
+                            $insert = true;
+                            foreach( $schema["primaryKey"] as $primary )
+                            {
+                                unset( $row[$encoded][$primary] );
+                            }
+                        }
+                    }
+                    
+                    if( $insert )
+                    {
+                        $dbObject->insert( $row );
+                    }
+                    else
+                    {
+                        $dbObject->update( $row );
+                    }
 				}
+				$this->showCatalog();
+                break;
 
 			default:
 				$this->showCatalog();
@@ -104,6 +127,17 @@ class DbEditor
 		}
 		return $where;
 	}
+	
+    public function deleteRecord( $keyArray )
+    {
+        $sql = sprintf( "DELETE FROM %s WHERE %s",
+            $this->dbObject->getTable(),
+            $this->getPrimaryKeyValueEncodedAsWhereClause( $keyArray ) );
+        $this->db->query( $sql );
+        print "<p>Deleting records...";
+        print "<br/><small><i>$sql</i></small>";
+        print "</p>";
+    }
 
 	// Takes an array of the form [ key1val, key2val ] in the order of
 	// $this->schema["primaryKey"];
@@ -111,23 +145,77 @@ class DbEditor
 	{
 		$db = $this->db;
 		$dbObject = $this->dbObject;
-		$sql = sprintf( "SELECT * FROM `%s` WHERE %s;",
-			$dbObject->getTable(),
-			$this->getPrimaryKeyValueEncodedAsWhereClause( $keyArray ) );
+        
+		$schema = $dbObject->getSchema();
+        list( $sqlFields, $sqlJoins ) = $this->buildJoins( $schema );
 
-		$row = $db->row( $sql );
-		$table = $dbObject->getTable();
-		$primaryKey = $this->getPrimaryKeyValueEncoded($row);
+        //$sql = sprintf( "SELECT %s FROM %s %s WHERE %s",
+            //$sqlFields, $dbObject->getTable(), $sqlJoins,
+			//$this->getPrimaryKeyValueEncodedAsWhereClause( $keyArray ) );
+		
+        $table = $dbObject->getTable();
+
+        $sql = "";
+        $row = array();
+        $primaryKey = "new";
+        if( $keyArray )
+        {
+            $sql = sprintf( "SELECT * FROM `%s` WHERE %s;",
+                $table,
+                $this->getPrimaryKeyValueEncodedAsWhereClause( $keyArray ) );
+            $row = $db->row( $sql );
+            $primaryKey = $this->getPrimaryKeyValueEncoded($row);
+        }
+        else
+        {
+            foreach( $schema["fields"] as $field )
+            {
+                $value = "";
+                if( in_array( $field, $schema["primaryKey"] ) )
+                    $value = "new";
+                $row[$field] = $value;
+            }
+        }
+
 		printf("<form method='POST' action='%s'>", $this->postURL);
 		print "<input name='operation' value='save' type='hidden'/>";
 		print "<table cellspacing='10'>";
-		print "<tr><th colspan='2'>Edit Record</th></tr>";
-		print "<tr><th colspan='2'><small><i>$sql</i></small></th></tr>";
+        if( $keyArray )
+        {
+            print "<tr><th colspan='2'>Edit Record</th></tr>";
+            print "<tr><th colspan='2'><small><i>$sql</i></small></th></tr>";
+        }
+        else
+        {
+            print "<tr><th colspan='2'>New Record</th></tr>";
+        }
 		foreach( $row as $key => $value )
 		{
+            $name = "records[$table][$primaryKey][$key]";
 			print "<tr>";
 			print "<th align='right'>$key: </th>";
-			print "<td><input name='records[$table][$primaryKey][$key]' value='$value'/></td>";
+			print "<td>";
+            if( array_key_exists( $key, $schema["foreignKeys"] ) )
+            {
+                $foreign = $schema["foreignKeys"][$key];
+                $sql = sprintf( "SELECT %s AS id, label FROM %s ORDER BY label",
+                    $foreign["referenced_column_name"], $foreign["referenced_table_name"] );
+                print "<select name='$name'>";
+                foreach( $db->rows( $sql ) as $entry )
+                {
+                    $selected = "";
+                    if( $entry["id"] == $value )
+                        $selected = "selected='selected'";
+                    printf( "<option value='%d' $selected>%s</option>",
+                        $entry["id"], $entry["label"] );
+                }
+                print "</select>";
+            }
+            else
+            {
+                print "<input name='$name' value='$value'/>";
+            }
+            print "</td>";
 			print "</tr>";
 		}
 		print "<tr>";
@@ -146,24 +234,13 @@ class DbEditor
 		return $field;
 	}
 
-	public function showCatalog()
-	{
-		$db = $this->db;
-		$dbObject = $this->dbObject;
-
-		$schema = $dbObject->getSchema();
-
-		print "<table>";
-
+    public function buildJoins( &$schema )
+    {
+        // Returns array( "field1, field2", "LEFT JOIN z ON x.a = z.a LEFT JOIN ...." )
         $sqlFields = "";
         $sqlJoins = "";
-
-		print "<tr>";
 		foreach( $schema["fields"] as $field )
-		{
-			$printable = $this->beautifyField( $field );
-			print "<th>$printable</th>";
-
+        {
             # Also build the SQL
             if( $sqlFields ) $sqlFields .= ", ";
             if( $sqlJoins ) $sqlJoins .= " ";
@@ -181,24 +258,63 @@ class DbEditor
                 $sqlFields .= sprintf( "%s.label AS %s",
                     $foreign["referenced_table_name"], $field );
             }
+        }
+        return array( $sqlFields, $sqlJoins );
+    }
+
+	public function showCatalog()
+	{
+		$db = $this->db;
+		$dbObject = $this->dbObject;
+
+		$schema = $dbObject->getSchema();
+
+        print "<script type='text/javascript'>
+            function confirmDelete( url )
+            {
+                if( confirm( 'Are you sure you want to delete this record?' ) )
+                {
+                    window.location = url;
+                }
+            }
+            </script>";
+		print "<table cellpadding='4' cellspacing='0' >";
+
+		print "<tr>";
+		foreach( $schema["fields"] as $field )
+		{
+			$printable = $this->beautifyField( $field );
+			print "<th>$printable</th>";
 		}
 		print "</tr>";
+        
+        list( $sqlFields, $sqlJoins ) = $this->buildJoins( $schema );
 
         $sql = sprintf( "SELECT %s FROM %s %s",
             $sqlFields, $dbObject->getTable(), $sqlJoins );
-        
+      
+        $colora = "#eeeaee";
+        $colorb = "#f2f3f2";
+        $color = $colorb;
 		foreach( $db->rows( $sql ) as $row )
 		{
 			$editLink = sprintf('<a href="%s?operation=edit&key=%s">edit</a>',
 				$this->postURL,
 				$this->getPrimaryKeyValueEncoded($row));
+			
+            $deleteLink = sprintf('<a href="javascript:confirmDelete(\'%s?operation=delete&key=%s\');">delete</a>',
+				$this->postURL,
+				$this->getPrimaryKeyValueEncoded($row));
+
+            $color = $color == $colora ? $colorb : $colora;
 
 			print "<tr>";
 			foreach( $schema["fields"] as $field )
 			{
-				print "<td>" . $row[$field] . "</td>";
+				print "<td bgcolor='$color'>" . $row[$field] . "</td>";
 			}
 			print "<td>$editLink</td>";
+			print "<td>$deleteLink</td>";
 			print "</tr>";
 		}
 
