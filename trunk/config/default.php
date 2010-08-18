3127d5db250b755e542e5db07569f277192b7794
cjastram@gmail.com <cjastram@gmail.com@918a0ca0-fd7c-11de-b9d1-2d015cf73129>
2010-08-18 13:58:41 +0000

    Updating configuration information

--- trunk/config/default.php
+++ trunk/config/default.php
@@ -3,3 +3,8 @@
 $specific_name = CONFIG_DIR . "/" . $_SERVER["SERVER_NAME"] . ".php";
 
 trigger_error("Need to create config file $specific_name", E_USER_ERROR);
+<?php
+
+$specific_name = CONFIG_DIR . "/" . $_SERVER["SERVER_NAME"] . ".php";
+
+trigger_error("Need to create config file $specific_name", E_USER_ERROR);
--- trunk/config/localhost.php
+++ trunk/config/localhost.php
@@ -4,5 +4,13 @@ $config = array(
 	"DB_HOST" => "localhost",
 	"DB_LOGIN" => "root",
 	"DB_PASSWORD" => "tgbnhy",
+	"DB_DATABASE" => "lg"
+);
+<?php
+
+$config = array(
+	"DB_HOST" => "localhost",
+	"DB_LOGIN" => "root",
+	"DB_PASSWORD" => "tgbnhy",
 	"DB_DATABASE" => "ticketing"
 );
